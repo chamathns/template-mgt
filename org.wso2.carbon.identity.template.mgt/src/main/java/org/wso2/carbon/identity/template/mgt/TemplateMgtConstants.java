55c0a31d13db1067f67a69c1ab505a9b94db37b3
chamath <chamath.15@cse.mrt.ac.lk>
2018-10-31 11:36:07 +0530

    add wso2 copyright to profiles, test model Template

--- org.wso2.carbon.identity.template.mgt/src/main/java/org/wso2/carbon/identity/template/mgt/TemplateMgtConstants.java
+++ org.wso2.carbon.identity.template.mgt/src/main/java/org/wso2/carbon/identity/template/mgt/TemplateMgtConstants.java
@@ -1,17 +1,19 @@
 /*
  * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.wso2.carbon.identity.template.mgt;
 
@@ -33,7 +35,7 @@ public class TemplateMgtConstants {
     public static final String TEMPLATE_RESOURCE_PATH = "/" + "templates";
 
     public static class SqlQueries{
-        public static final String INSERT_TEMPLATE = "INSERT INTO IDN_TEMPLATE_MGT (TENANT_ID, NAME, DESCRIPTION, TEMPLATE_SCRIPT) VALUES (?,?,?,?)";
+        public static final String INSERT_TEMPLATE = "INSERT INTO IDN_TEMPLATE_MGT (TENANT_ID, NAME, DESCRIPTION, TEMPLATE_SCRIPT) VALUES (?,?,?,?  )";
         public static final String GET_TEMPLATE_BY_NAME = "SELECT TEMPLATE_ID,TENANT_ID,NAME,DESCRIPTION,TEMPLATE_SCRIPT FROM IDN_TEMPLATE_MGT WHERE NAME=? AND TENANT_ID=?";
 //        public static final String LIST_TEMPLATES = "SELECT NAME,DESCRIPTION FROM IDN_TEMPLATE_MGT WHERE TENANT_ID =?";
         public static final String LIST_PAGINATED_TEMPLATES_MYSQL = "SELECT NAME,DESCRIPTION FROM IDN_TEMPLATE_MGT WHERE TENANT_ID=? ORDER BY TEMPLATE_ID ASC LIMIT ? OFFSET ?";
---
+++ org.wso2.carbon.identity.template.mgt/src/test/java/org/wso2/carbon/identity/template/mgt/model/TemplateTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.template.mgt.model;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class TemplateTest {
+    private static Integer templateId = 1;
+    private static Integer tenantId = -1234;
+    private static final String templateName = "sampleTemplate";
+    private static final String description = "sample Description";
+    private static final String script = "sample Script";
+
+    Template template = new Template(templateId,tenantId,templateName,description,script);
+
+    @Test
+    public void testGetTemplateId() {
+        Assert.assertEquals(template.getTemplateId(),templateId);
+    }
+
+    @Test
+    public void testSetTemplateId() {
+        Integer templateId = 101;
+        template.setTemplateId(templateId);
+        Assert.assertEquals(template.getTemplateId(),templateId);
+    }
+
+    @Test
+    public void testGetTenantId() {
+        Assert.assertEquals(template.getTenantId(),tenantId);
+    }
+
+    @Test
+    public void testSetTenantId() {
+        Integer tenantId = 101;
+        template.setTenantId(tenantId);
+        Assert.assertEquals(template.getTenantId(),tenantId);
+    }
+
+    @Test
+    public void testGetTemplateName() {
+        Assert.assertEquals(template.getTemplateName(),templateName);
+    }
+
+    @Test
+    public void testSetTemplateName() {
+        String templateName = "test Name";
+        template.setTemplateName(templateName);
+        Assert.assertEquals(template.getTemplateName(),templateName);
+    }
+
+    @Test
+    public void testGetDescription() {
+        Assert.assertEquals(template.getDescription(),description);
+    }
+
+    @Test
+    public void testSetDescription() {
+        String description = "test description";
+        template.setDescription(description);
+        Assert.assertEquals(template.getDescription(),description);
+    }
+
+    @Test
+    public void testGetTemplateScript() {
+        Assert.assertEquals(template.getTemplateScript(),script);
+
+    }
+
+    @Test
+    public void testSetTemplateScript() {
+        String script = "test script";
+        template.setTemplateScript(script);
+        Assert.assertEquals(template.getTemplateScript(),script);
+    }
+}
